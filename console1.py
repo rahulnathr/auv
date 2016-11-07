2d0b40a22a56c082e5f190572bfabd97aa447aa2
Rahulnath.R <rahulnath2473@gmail.com>
2016-11-07 17:00:16 +0530

    Add files via upload

---
+++ console1.py
@@ -0,0 +1,29 @@
+import os 
+import os.path 
+import sys
+from pymavlink import mavutil 
+import errno
+import time 
+
+from MAVProxy.modules.lib import mp_module 
+from MAVProxy.modules.lib import mp_util
+from MAVProxy.modules.lib import mp_settings 
+
+#initialising the module
+class console1(mp_module.MPModule):
+	def __init__(self,mpstate):
+		super(console1,self).__init__(mpstate,"console1","arming the px4")
+
+		self.master.arducopter_arm()    #arm the module 
+		channels=self.override
+		self.set_override(channels[4]=1450)
+		
+										#sleep again
+
+		
+	
+
+
+def init(mpstate):
+	'''initialise module'''
+	return console1(mpstate)
